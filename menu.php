711e83e8d8685081ef6d72af95de7cbcd79a1b8b
Zag <85066044+za12ew44zz@users.noreply.github.com>
2024-05-07 13:35:46 +0700

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="jquery3.7.1.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    
+    
+
+    
+    <nav class="navbar"><h1 class="logo"><img src="./images/logo.png" class="logo-img" alt="" width="60px"; height="60px">........ Admin Dashboard</h1>
+    
+    
+    <div class="nav-item">
+        <ul>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+        </ul>
+    </div>
+</nav>
+
+
+    <h1 id="menu-function">|||</h1>
+    <div id="box-menu">
+        <ul>
+            <li id="full-screen">F</li>
+            <li>d</li>
+        </ul>
+    </div>
+
+    <ul class="menu">
+        <li></li>
+        <li></li>
+        <li class="list-menu" id="homePage">Home</li>
+        <li class="list-menu" id="lineCalulator">Tax line calculator</li>
+       
+        
+    </ul>
+
+
+
+
+
+
+
+    <script src="script.js"></script>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,44 @@
+const homePage = document.getElementById("homePage")
+const lineCalculator = document.getElementById("lineCalulator")
+
+
+
+// Start - menu bar //
+
+homePage.addEventListener("click",function(){
+    window.location.href = "index.php"
+    
+    
+})
+
+lineCalculator.addEventListener("click",function(){  
+    window.open("https://za12ew44zz.github.io/Tax-Calculator-Line-Creator/","_blank")
+  
+})
+
+// End - menu bar //
+
+
+
+
+
+$(document).ready(function(){
+    $("#menu-function").click(function(){
+      
+        $("#box-menu").toggle(100)
+    })
+
+    $("#full-screen").click(function(){
+        if (document.fullscreenEnabled) {
+           
+            document.documentElement.requestFullscreen();
+          } else if (document.webkitFullscreenEnabled) {  // Safari
+            document.documentElement.webkitRequestFullscreen();
+          } else if (document.msFullscreenEnabled) {  // IE/Edge
+            document.documentElement.msRequestFullscreen();
+          } else {
+           
+            console.error("Fullscreen not supported by your browser.");
+          }
+    })
+})
